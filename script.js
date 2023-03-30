0ce724abd7ac9fa2e0fef38eb79a2b1202c0a813
PravinElangovan <115397005+PravinElangovan@users.noreply.github.com>
2023-03-30 19:39:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
-//your code here
-const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
+// Get all the prices from the table
+const prices = document.querySelectorAll('[data-ns-test="prices"]');
 
-const getSum = () => {
-//Add your code here
-	let prices = document.querySelectorAll('.price');
-	let sum = 0;
-	prices.forEach(price => {
-		sum += parseFloat(price.textContent);
-	});
-	document.getElementById('table').innerHTML += `
-	   <tr id="ans">
-          <td></td>
-          <td>${sum}</td>
-        </tr>`
-};
+// Calculate the sum of prices
+let sum = 0;
+prices.forEach(price => {
+  sum += parseInt(price.textContent);
+});
 
-getSumBtn.addEventListener("click", getSum)
+// Set the sum as the value of the last cell
+const grandTotal = document.querySelector('[data-ns-test="grandTotal"]');
+grandTotal.textContent = sum.toString();
