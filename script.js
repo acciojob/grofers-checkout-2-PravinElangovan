b93facea3c99c6783e0e33ed72b0b1d3b9b10e76
PravinElangovan <115397005+PravinElangovan@users.noreply.github.com>
2023-03-30 19:42:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-// Get all the prices from the table
-const prices = document.querySelectorAll('[data-ns-test="prices"]');
+function calculateTotal() {
+  const prices = Array.from(document.querySelectorAll('[data-ns-test="prices"]')).map((price) => parseFloat(price.textContent));
+  const grandTotal = document.querySelector('[data-ns-test="grandTotal"]');
+  
+  // Check if the number of prices is correct
+  if (prices.length !== 4) {
+    console.error('Incorrect number of prices');
+    return;
+  }
+  
+  const totalPrice = prices.reduce((acc, price) => acc + price, 0);
+  
+  // Check if the total price is correct
+  if (totalPrice !== parseFloat(grandTotal.textContent)) {
+    console.error('Incorrect total price');
+    return;
+  }
+  
+  // Add a new row to the table with the total price
+  const newRow = document.createElement('tr');
+  const newCell = document.createElement('td');
+  newCell.textContent = `Total: ${totalPrice.toFixed(2)}`;
+  newRow.appendChild(newCell);
+  grandTotal.parentElement.parentElement.appendChild(newRow);
+}
 
-// Calculate the sum of prices
-let sum = 0;
-prices.forEach(price => {
-  sum += parseInt(price.textContent);
-});
-
-// Set the sum as the value of the last cell
-const grandTotal = document.querySelector('[data-ns-test="grandTotal"]');
-grandTotal.textContent = sum.toString();
+calculateTotal();
